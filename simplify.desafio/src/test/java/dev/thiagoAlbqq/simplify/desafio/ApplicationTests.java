1bfcd34a6c510ab5ea50ef486c5aae8f695bf3f4
Bowie <thiago.ce.al@gmail.com>
2025-03-14 00:01:08 -0300

    Estruturação do projeto

---
+++ simplify.desafio/src/test/java/dev/thiagoAlbqq/simplify/desafio/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagoAlbqq.simplify.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
