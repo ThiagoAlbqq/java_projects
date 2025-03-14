b70e1edcc20f1ae1d4a4e1b8caeaa3a896bf966f
Bowie <thiago.ce.al@gmail.com>
2025-03-14 00:10:20 -0300

    Iniciando ENTITY Todo

---
+++ simplify.desafio/src/main/java/dev/thiagoAlbqq/simplify/desafio/entity/Todo.java
@@ -0,0 +1,59 @@
+package dev.thiagoAlbqq.simplify.desafio.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+    public Todo(int prioridade, boolean realizado, String descricao, String nome) {
+        this.prioridade = prioridade;
+        this.realizado = realizado;
+        this.descricao = descricao;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
